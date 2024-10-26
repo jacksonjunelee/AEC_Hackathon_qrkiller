a235ef566d78858a3e5db8eb7d4b9fb9a8a43771
travispotterBH <travis.s.potter@gmail.com>
2024-10-26 15:21:54 -0400

    asp.net project

---
+++ server/QRKiller/QRKiller/Controllers/ImageController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace QRKiller.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImageController : ControllerBase
+    {
+        private readonly ImageService _imageService;
+
+        public ImageController(ImageService imageService)
+        {
+            _imageService = imageService;
+        }
+
+        [HttpGet("{imageName}")]
+        public async Task<IActionResult> GetImage(string imageName)
+        {
+            try
+            {
+                byte[] imageBytes = _imageService.GetImage(imageName);
+                return File(imageBytes, "image/png");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+
+}
