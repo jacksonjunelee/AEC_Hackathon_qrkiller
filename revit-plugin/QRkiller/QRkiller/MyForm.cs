39b99f79690f689266a082820a1379b9c838a299
Res0lu7ion <38870927+Res0lu7ion@users.noreply.github.com>
2024-10-26 23:11:13 -0400

    n

---
+++ revit-plugin/QRkiller/QRkiller/MyForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Collections.Generic;
+using System.Windows;
+
+
+public class MyForm
+{
+	public MyForm()
+	{
+
+	}
+}
