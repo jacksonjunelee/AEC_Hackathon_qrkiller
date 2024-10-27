2fec4544bb2427407158f76cfb10df108afc39a9
Res0lu7ion <38870927+Res0lu7ion@users.noreply.github.com>
2024-10-26 21:30:12 -0400

    Update Class1.cs

--- revit-plugin/QRkiller/QRkiller/Class1.cs
+++ revit-plugin/QRkiller/QRkiller/Class1.cs
@@ -1,7 +1,35 @@
-﻿namespace QRkiller
-{
-    public class Class1
-    {
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 
+namespace QRkiller
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Class1 : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+;        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+
+    public class Class2 : IExternalCommand
+    {
+        //Fill in with code
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // MyForm form = new MyForm();
+            //form.ShowDialog();
+
+            TaskDialog.Show("My Message", "Hello, this is a message from your Revit test add-in.");
+            return Result.Succeeded;
+
+        }
     }
 }
