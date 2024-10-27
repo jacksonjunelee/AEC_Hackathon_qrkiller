88a66948eb54fd9928052351548060592b2e3970
Res0lu7ion <38870927+Res0lu7ion@users.noreply.github.com>
2024-10-27 00:09:41 -0400

    n
    n

--- revit-plugin/QRkiller/QRkiller/Class1.cs
+++ revit-plugin/QRkiller/QRkiller/Class1.cs
@@ -2,47 +2,108 @@
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
+using static System.Collections.Specialized.BitVector32;
 
-namespace Lab1PlaceGroup
+namespace QRKiller.Revit
 {
     [Transaction(TransactionMode.Manual)]
-    public class ReferForm : IExternalCommand
+    public class Class1 : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             UIDocument uidoc = commandData.Application.ActiveUIDocument;
             Document doc = commandData.Application.ActiveUIDocument.Document;
-            DocumentPreviewSettings settings = doc.GetDocumentPreviewSettings();
-            //
-            string info = "";
-            foreach (View3D v in new FilteredElementCollector(doc).OfClass(typeof(View3D)))
-            {
-                if (v.Name == "{3D}")
-                {
-                    uidoc.ActiveView = v;
-                }
-            }
-            //
+            SetPlanView(doc, uidoc);
+
+            View activeView = doc.ActiveView;
             Selection selection = uidoc.Selection;
-            ICollection<ElementId> selectedIds = uidoc.Selection.GetElementIds();
-            bool t = false;
-            if (selectedIds.Count == 0)
+            ICollection<ElementId> selectedIds = selection.GetElementIds();
+            FilteredElementCollector collector = new FilteredElementCollector(doc)
+            .OfClass(typeof(Viewport))
+            .WhereElementIsNotElementType();
+
+            foreach (Viewport viewport in collector)
             {
-                TaskDialog.Show("Revit", "You haven't selected any elements at all.");
+                View view = doc.GetElement(viewport.ViewId) as View;
+                TaskDialog.Show("view names", "req view: " + view.Name + "\n" + view.Name);
+                // Process the embedded viewport, such as focusing or exporting
             }
-           // else
-          //  {
-          //      string s2 = ProcessSelection(doc, uidoc, selectedIds);
-             //   TaskDialog.Show("Revit", "data copied to clipboard / write json to file\n" + s2);
-             //   t = true;
+
+            //DocumentPreviewSettings settings = doc.GetDocumentPreviewSettings();
+            ////
+            //string info = "";
+            //foreach (View3D v in new FilteredElementCollector(doc).OfClass(typeof(View3D)))
+            //{
+            //    if (v.Name == "{3D}")
+            //    {
+            //        uidoc.ActiveView = v;
+            //    }
             //}
-            if (t)
-            {
-                // drawInRhino();
-            }
+            ////
+            //Selection selection = uidoc.Selection;
+            //ICollection<ElementId> selectedIds = uidoc.Selection.GetElementIds();
+            //bool t = false;
+            //if (selectedIds.Count == 0)
+            //{
+            //    TaskDialog.Show("Revit", "You haven't selected any elements at all.");
+            //}
+            // else
+            //  {
+            //      string s2 = ProcessSelection(doc, uidoc, selectedIds);
+            //   TaskDialog.Show("Revit", "data copied to clipboard / write json to file\n" + s2);
+            //   t = true;
+            //}
+            // if (t)
+            // {
+            // drawInRhino();
+            //  }
             return Result.Succeeded;
         }
-    }
+
+        private void SetPlanView(Document doc, UIDocument uidoc)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc).OfClass(typeof(ViewPlan));
+            ViewPlan viewPlan = null;
+            string reqView = "";
+            string viewNames = "";
+            foreach (ViewPlan v in collector)
+            {
+                viewNames += v.Name + ", ";
+                if (v.Name.Contains("Level 1"))
+                {
+                    reqView = v.Name;
+                    viewPlan = v;
+                    uidoc.ActiveView = viewPlan;
+                    break;
+                }
+            }
+
+            TaskDialog.Show("view names", "req view: " + reqView + "\n" + viewNames);
+        }
+
+        public void set3dView(Autodesk.Revit.DB.Document doc, UIDocument uidoc)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc).OfClass(typeof(View3D));
+            View3D view3d = null;
+            string reqView = "";
+            string viewNames = "";
+            foreach (View3D v in collector)
+            {
+                viewNames += v.Name + ", ";
+                if (v.Name.Contains("{3D}"))
+                {
+                    reqView = v.Name;
+                    view3d = v;
+                    uidoc.ActiveView = view3d;
+                    break;
+                }
+            }
+
+            TaskDialog.Show("view names", "req view: " + reqView + "\n" + viewNames);
+        }
+
+
+        }
 }
 //namespace QRkiller
 //{
